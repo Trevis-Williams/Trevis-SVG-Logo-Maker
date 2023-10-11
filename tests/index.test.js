a0eb1e566a1764a4adbf41a000a65d410d5b91a3
Developer ✓ <rst1426@gmail.com>
2023-10-11 12:52:44 -0700

    trying things

--- tests/index.test.js
+++ tests/index.test.js
@@ -1,8 +1,9 @@
-const { generateSVG } = require('../lib/index.js'); // Adjust the path as needed
-const fs = require('fs');
+import { generateSVG } from '../lib/index.mjs'; // Adjust the path as needed
+import fs from 'fs';
 
 test('Generate SVG with provided input', () => {
   generateSVG('ABC', 'red', 'Circle', 'blue');
   const svgExists = fs.existsSync('logo.svg');
   expect(svgExists).toBe(true);
 });
+
