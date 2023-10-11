dea2bbfb414bc8ae2b556d4b89b35697acccb4dc
Trevis Williams <rst1426@gmail.com>
2023-10-11 11:36:47 -0700

    updated shapes.js

--- tests/index.test.js
+++ tests/index.test.js
@@ -1,4 +1,4 @@
-const { generateSVG } = require('../lib/index.mjs'); // Adjust the path as needed
+const { generateSVG } = require('../lib/index.js'); // Adjust the path as needed
 const fs = require('fs');
 
 test('Generate SVG with provided input', () => {
